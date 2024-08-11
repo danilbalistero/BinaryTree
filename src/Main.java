60eee828eb840037932583aa500fedf3d32b4dc1
danilbalistero <danilbalistero@gmail.com>
2024-08-11 14:23:56 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArvoreBinaria arvoreBinaria = new ArvoreBinaria();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        arvoreBinaria.inserir(80);
+        arvoreBinaria.inserir(72);
+        arvoreBinaria.inserir(87);
+        arvoreBinaria.inserir(37);
+        arvoreBinaria.inserir(-3);
+        arvoreBinaria.inserir(12);
+        arvoreBinaria.preOrdem(arvoreBinaria.getRaiz());
+        System.out.println("----------------------------");
+        arvoreBinaria.emOrdem(arvoreBinaria.getRaiz());
+        System.out.println("----------------------------");
+        arvoreBinaria.posOrdem(arvoreBinaria.getRaiz());
+
     }
 }
